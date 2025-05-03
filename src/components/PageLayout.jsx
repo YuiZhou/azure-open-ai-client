d2b2fd2e80cd4e4a908c5b58a3635b43535ef9ea
Yuwei Zhou <yuwzho@microsoft.com>
2025-05-03 14:59:17 +0800

    Sign in and Sign out

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -20,19 +20,11 @@ export const PageLayout = (props) => {
 
     return (
         <>
-            <Navbar bg="primary" variant="dark" className="navbarStyle">
-                <Link className="navbar-brand" to="/">
-                    Microsoft Identity Platform
-                </Link>
+            <Navbar bg="transparent" variant="light" className="navbarStyle">
                 <div className="collapse navbar-collapse justify-content-end">
                     {isAuthenticated ? <SignOutButton /> : <SignInButton />}
                 </div>
             </Navbar>
-                <div className="title">
-                    <h5>
-                        Welcome to the Microsoft Authentication Library For JavaScript - React SPA
-                    </h5>
-                </div>
                 <div className="profileContent">
                     {props.children}
                 </div>
--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -1,32 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useMsal } from "@azure/msal-react";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
+import { loginRequest } from "../authConfig";
+import { callMsGraph } from "../graph";
 
 /**
- * Renders a sign-out button
+ * Renders a sign-out button with the user's name and toggle dropdown
  */
 export const SignOutButton = () => {
-    const { instance } = useMsal();
+    const { instance, accounts } = useMsal();
+    const [userName, setUserName] = useState("User");
+    const [graphData, setGraphData] = useState(null);
 
-    const handleLogout = (logoutType) => {
-        if (logoutType === "popup") {
-            instance.logoutPopup({
-                postLogoutRedirectUri: "/",
-                mainWindowRedirectUri: "/"
-            });
-        } else if (logoutType === "redirect") {
-            instance.logoutRedirect({
-                postLogoutRedirectUri: "/",
+    useEffect(() => {
+        if (accounts.length > 0) {
+            // Set the username from account information if available
+            setUserName(accounts[0].name || accounts[0].username || "User");
+            
+            // Get access token to call Microsoft Graph API
+            instance.acquireTokenSilent({
+                ...loginRequest,
+                account: accounts[0]
+            }).then((response) => {
+                callMsGraph(response.accessToken)
+                    .then(response => {
+                        setGraphData(response);
+                        // Update username with more accurate data from Graph API
+                        if (response.displayName) {
+                            setUserName(response.displayName);
+                        }
+                    });
+            }).catch(error => {
+                console.log(error);
             });
         }
-    }
+    }, [accounts, instance]);
 
+    const handleLogout = () => {
+        instance.logoutRedirect({
+            postLogoutRedirectUri: "/",
+        });
+    }
     
     return (
-        <DropdownButton variant="secondary" className="ml-auto" drop="start" title="Sign Out">
-            <Dropdown.Item as="button" onClick={() => handleLogout("popup")}>Sign out using Popup</Dropdown.Item>
-            <Dropdown.Item as="button" onClick={() => handleLogout("redirect")}>Sign out using Redirect</Dropdown.Item>
+        <DropdownButton 
+            variant="transparent" 
+            className="ml-auto" 
+            drop="down" 
+            align="end"
+            title={userName}
+        >
+            {graphData && (
+                <Dropdown.Item disabled>
+                    {graphData.userPrincipalName}
+                </Dropdown.Item>
+            )}
+            <Dropdown.Divider />
+            <Dropdown.Item as="button" onClick={handleLogout}>
+                Sign out
+            </Dropdown.Item>
         </DropdownButton>
     )
 }
